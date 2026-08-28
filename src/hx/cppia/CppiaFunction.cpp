f40c099cbba902f9f122726ceadd61057d1077b1
Kade-gtihub <26305836+Kade-github@users.noreply.github.com>
2026-08-28 12:46:31 -0700

    smarter unloading

--- src/hx/cppia/CppiaFunction.cpp
+++ src/hx/cppia/CppiaFunction.cpp
@@ -12,6 +12,12 @@ static int sTypeSize[] = { 0, 0, sizeof(hx::Object *), sizeof(String), sizeof(Fl
 
 String sInvalidArgCount = HX_CSTRING("Invalid arguement count");
 
+String sUnloadedMessage = HX_CSTRING("Called a function from a cppia class that has been unloaded");
+
+#ifdef CPPIA_JIT
+static void SLJIT_CALL unloadedCompiled(CppiaCtx *ctx);
+#endif
+
 
 #ifdef CPPIA_JIT
 void SLJIT_CALL argToInt(CppiaCtx *ctx) { ctx->pushInt( (* (hx::Object **)(ctx->pointer))->__ToInt() ); }
@@ -143,7 +149,7 @@ ScriptCallable::ScriptCallable(CppiaModule &inModule,ScriptNamedFunction *inFunc
 ScriptCallable::~ScriptCallable()
 {
    #ifdef CPPIA_JIT
-   if (compiled)
+   if (compiled && compiled != unloadedCompiled)
       CppiaCompiler::freeCompiled(compiled);
    #endif
 }
@@ -687,17 +693,31 @@ struct UnloadedBody : public CppiaExpr
 
    void runVoid(CppiaCtx *ctx) HXCPP_OVERRIDE
    {
-      hx::Throw( HX_CSTRING("Called a function from a cppia class that has been unloaded") );
+      hx::Throw( sUnloadedMessage );
    }
+
+   int         runInt(CppiaCtx *ctx) HXCPP_OVERRIDE { runVoid(ctx); return 0; }
+   Float       runFloat(CppiaCtx *ctx) HXCPP_OVERRIDE { runVoid(ctx); return 0; }
+   ::String    runString(CppiaCtx *ctx) HXCPP_OVERRIDE { runVoid(ctx); return null(); }
+   hx::Object *runObject(CppiaCtx *ctx) HXCPP_OVERRIDE { runVoid(ctx); return 0; }
 };
 
 static UnloadedBody sUnloadedBody;
+
+#ifdef CPPIA_JIT
+static void SLJIT_CALL unloadedCompiled(CppiaCtx *ctx)
+{
+   TRY_NATIVE
+   hx::Throw( sUnloadedMessage );
+   CATCH_NATIVE
+}
+#endif
 
 void ScriptCallable::deactivate()
 {
    body = &sUnloadedBody;
    #ifdef CPPIA_JIT
-   compiled = 0;
+   compiled = unloadedCompiled;
    #endif
 }
 
